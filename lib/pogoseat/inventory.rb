a38ed854bea3e70b517935fa96a6d5c6797720d1
Phil Crumm <phil@pogoseat.com>
2015-10-15 17:26:57 -0700

    When searching for inventory, return found inventory as `InventoryItem`s.

--- lib/pogoseat/inventory.rb
+++ lib/pogoseat/inventory.rb
@@ -7,7 +7,15 @@ module Pogoseat
     def self.all
       response = self.get '/inventory'
       self.check_response response
-      response
+   
+      # Instantiate each inventory item as an InventoryItem
+      inventory_items = []
+      response.each { |item_props|
+        new_inventory_item = Pogoseat::InventoryItem.new(item_props, false)
+        inventory_items.push new_inventory_item
+      }
+      
+      return inventory_items
     end
   end
 end
--- spec/inventory_spec.rb
+++ spec/inventory_spec.rb
@@ -4,7 +4,23 @@ describe Pogoseat::Inventory do
   describe "#all" do
     it "should not list any inventory when none exists" do
       inventory_list = Pogoseat::Inventory.all
-      inventory_list.empty?
+      expect(inventory_list.length).to eq(0)
+    end
+
+    it "should return inventory as InventoryItems when inventory exists" do
+      # First, create an inventory item
+      inventory_property = { "title" => "Test Title", "price" => 5 }
+      new_item = Pogoseat::InventoryItem.new inventory_property
+      new_item.save!
+
+      # Now, fetch inventory...
+      inventory_items = Pogoseat::Inventory.all
+      expect(inventory_items.count).to eq(1)
+      expect(inventory_items[0]).to be_a Pogoseat::InventoryItem
+      expect(inventory_items[0].title).to eq("Test Title")
+      
+      # Let's do some cleanup and delete our inventory item
+      new_item.delete!
     end
   end
 end
