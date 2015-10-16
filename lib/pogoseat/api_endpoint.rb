b2e6fa6f219651f8eed1be2130f4994d3cc1d21a
Phil Crumm <phil@pogoseat.com>
2015-10-15 17:26:13 -0700

    Add helper function for API models which will add the parameters as a given hash as instance variables for the current class.

--- lib/pogoseat/api_endpoint.rb
+++ lib/pogoseat/api_endpoint.rb
@@ -3,10 +3,30 @@ module Pogoseat
     include HTTParty
     base_uri Pogoseat::API_BASE_URL
     basic_auth ENV["POGOSEAT_API_KEY"], ENV["POGOSEAT_API_SECRET"]
+    format :json
 
+    # A helper function to verify that the API response appears successful.
     def self.check_response response
       if response.code != 200
         raise "API authentication failed"
+      end
+    end
+
+    protected
+    # Sets each member of the opts hash as an instance variable.
+    def set_instance_variables opts
+      opts.each do |k,v|
+        begin
+          instance_variable_set "@#{k}", v
+
+          # Sets the accessor for the property as well
+          eigenclass = class<<self; self; end
+          eigenclass.class_eval do
+            attr_accessor k
+          end
+        rescue NameError => e
+          # We'll just eat the error for now
+        end
       end
     end
   end
